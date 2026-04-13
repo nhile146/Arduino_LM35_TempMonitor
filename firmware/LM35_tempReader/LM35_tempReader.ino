38cc40966a57e41b88601dffd0cd51956a7f1b2b
nhile146 <lengocyennhi9723@gmail.com>
2026-04-13 16:08:31 +0700

    feat: ho tro doc 2 kenh LM35 (A0, A1)

--- firmware/LM35_tempReader/LM35_tempReader.ino
+++ firmware/LM35_tempReader/LM35_tempReader.ino
@@ -1,13 +1,24 @@
-int adcValue;
-float nhietDo;
-void setup(){
+int adcValues[2]; 
+float nhietDo[2]; 
+char chuoi[30];
+
+void setup() {
   Serial.begin(9600);
   delay(100);
 }
 
-void loop(){
-  adcValue = analogRead(A0);
-  nhietdo = (adcVAlue * 500.0) / 1023.0;
-  Serial.println(nhietDo,1);
-  delay(1000);
+void loop() {
+  // Doc kenh A0
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+  
+  // Doc kenh A1
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+  
+  // Dong goi thanh chuoi CSV va gui qua Serial
+  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+  
+  delay(100);
 }
